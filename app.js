01a4dedf87f2ead7cc9bd721938d2aeb13671a03
Evick <eviyanne@gmail.com>
2017-06-04 14:13:52 +0900

    トリボナッチ数列を出力するように変更

--- app.js
+++ app.js
@@ -3,19 +3,20 @@
 const memo = new Map();
 
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
 
 function fib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
 
-    const value = fib(n - 1) + fib(n - 2);
+    const value = fib(n - 1) + fib(n - 2) + fib(n - 3);
     memo.set(n, value);
     return value;
 }
 
-const length = 1000;
+const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(fib(i));
 }
