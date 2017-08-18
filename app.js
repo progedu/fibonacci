48abdf8ed3adc92bb4d441ef0f0ecec2ef2a58e8
Ryoto Kojima <ryoto.k@gmail.com>
2017-08-18 05:06:48 +0000

    練習問題実装完了

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ function trib(n) {
     if(memo.has(n)) {
         return memo.get(n);
     }
-    const value = tirb(n - 1) + tirb(n - 2) + trib(n - 3);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
 }
-const length = 40;
+const length = 10000;
 for (let i = 0; i <= length; i++) {
-    console.log(tirb(i));
+    console.log(trib(i));
 }
