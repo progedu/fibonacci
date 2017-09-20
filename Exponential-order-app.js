757489ec95dedf5c2df2a47770679c7a50e00e26
m-sayuri2525 <frawreau@yahoo.co.jp>
2017-09-20 08:32:29 +0000

    指数オーダーのfib関数の実装

---
+++ Exponential-order-app.js
@@ -0,0 +1,19 @@
+'use strict';
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fib(n-1) +fib(n-2);
+}
+
+const length = 40;
+for(let i=0; i<= length; i++){
+  console.log( fib(i));
+}
+
+/**
+ * Exponential order
+ * 40 loops , user    0m5.076s
+ */
