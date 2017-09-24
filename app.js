56aa2a32e188053c64eb17c49a950822e35bff53
yakuma <yakuma8@gmail.com>
2017-09-24 22:24:09 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+function fib(n){
+    if(n===0){
+        return 0;
+    }else if(n===1){
+        return 1;
+    }
+    const t0 = fib(n-1);
+    const t1 = fib(n-2);
+    const all = t0 + t1;
+    return all;
+}
+
+const length = 40;
+for(let i=0;i<length;i++){
+    console.log(fib(i));
+}
