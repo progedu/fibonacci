2bd6c5819738f0fc138462662ebf077d4d5ff37c
Yoshihisa Masuda <yossoon@gmail.com>
2017-10-21 04:22:33 +0000

    Exponential fib function implemented

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function fib(n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+const length = 40;
+for (let i = 0; i < length; i++) {
+    console.log(fib(i));
+}
