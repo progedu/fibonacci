a95a6150f6b7a821c5ad7bec3691fae78c9f90d1
minatooe <minatooe@gmail.com>
2017-12-15 14:20:53 +0000

    トリボナッチ化完了

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 'use strict';
-function fib(n){
-    if(n===0){
-        return 0;
-    }else if(n===1){
-        return 1;
+const memo=new Map();
+memo.set(0,0);
+memo.set(1,0);
+memo.set(2,1);
+
+function tri(n){
+    if(memo.has(n)){
+        return memo.get(n);
     }
-    return fib(n-1)+fib(n-2);
+    const value = tri(n-1)+tri(n-2)+tri(n-3);
+    memo.set(n,value);
+    return value;
 }
 
 const length = 40;
 for (let i = 0;i<=length;i++){
-    console.log(fib(i));
+    console.log(tri(i));
 }
