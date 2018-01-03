aa06778069410d0e4ccf66df324312d90ee46d89
fumiWT <fumi2543@outlook.jp>
2018-01-03 04:26:52 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+function fib(n){
+    if (n === 0){
+        return 0;
+    }
+    else if (n === 1){
+        return 1;
+    }
+    //最初のfib(0),fib(1)を定義しておけば、後は再帰するだけで勝手に計算してくれる
+    return fib(n - 1) + fib(n - 2);
+}
+const length = 40;
+for (let i = 0;i<=length;i++){
+    console.log(fib(i));
+}
