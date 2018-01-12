a34889417393ef18cfc27959946c91f2ba24b39e
wadayym <wadayym@gmail.com>
2018-01-12 13:52:39 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n){
+    if(n===0){
+        return 0;
+    }else if(n===1){
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
+const lingth=40;
+for(let i=0;i<=lingth;i++){
+    console.log(fib(i));
+}
