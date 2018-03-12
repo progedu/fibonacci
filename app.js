b4b7821f410abdcac0faf7f3cc90be33430d1f36
shiki346 <dsorange13@icloud.com>
2018-03-12 07:08:49 +0000

    トリボナッチ数列に対応（練習問題）

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 'use strict'
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
-function fib(n) {
+memo.set(1, 0);
+memo.set(2, 1)
+function trib(n) {
     if(memo.has(n)) {
         return memo.get(n);
     }
-    const value = fib(n - 1) + fib(n - 2);
-    memo.set(n,value);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+    memo.set(n, value);
     return value;
 }
 const length = 40;
