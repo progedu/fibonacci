f21068ca2f1fb2a32e070a56b464dcd33b7d0cdd
Satoshi Uematsu <satoshi_uem@yahoo.co.jp>
2018-05-30 22:35:24 +0000

    指数オーダのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n){
+    if(n==0){
+        return 0;
+    }else if(n==1){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+const length = 40;
+for (let index = 0; index < length; index++) {
+    console.log(fib(index));
+}
