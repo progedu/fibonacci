dc577b19fd2ca26840c1e9ed9646b42375fa1842
hilolih <hilolih@gmail.com>
2018-10-06 13:15:41 +0900

    アルゴリズムの改善： 練習問題

--- app.js
+++ app.js
@@ -15,9 +15,24 @@ function fib(n) {
     memo.set(n, a);
     return a;
 }
+
+memo.set(2,1);
+
+function trib(n) {
+    let a = memo.get(n); 
+    if (a || a === 0) {
+        return a;
+    }
+    let n1 =fib(n-1);
+    let n2 = fib(n-2);
+    let n3 = fib(n-3);
+    a = n1 + n2 + n3;
+    memo.set(n, a);
+    return a;
+}
 const length = 40;
 
 
 for(let i=0; i <= length; i++) {
-    console.log(fib(i));
+    console.log(trib(i));
 }
