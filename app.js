1f6a283dd72c069a968827d92ba580635212556f
STSyns <sacculia@gmail.com>
2018-10-15 09:28:06 +0000

    トリボナッチ数列実装

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ function fib(n) {
     return value;
 }
 
+const memo2 = new Map();
+memo2.set(0, 0);
+memo2.set(1, 0);
+memo2.set(2, 1);
+function tri(n) {
+    if (memo2.has(n)) {
+        return memo2.get(n);
+    }
+    const value = tri(n - 1) + tri(n - 2) + tri(n - 3);
+    memo2.set(n, value);
+    return value;
+}
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
+    console.log(tri(i));
 }
