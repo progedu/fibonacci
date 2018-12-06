d7a758287565866e4d1204e2cb95e2fedc87ad24
fijie <logicool.civ4@gmail.com>
2018-12-06 08:43:45 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// 「フィボナッチ数列を40番目まで出力する」
+
 'use strict';
+
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++ ) {
+    console.log(fib(i));
+}
