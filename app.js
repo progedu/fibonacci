5cf986ac1d9c19855f871f4435b4124fbece072c
fijie <logicool.civ4@gmail.com>
2018-12-07 09:20:00 +0000

    関数の名称を変更

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-// 「フィボナッチ数列を40番目まで出力する」
-
 'use strict';
+
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
 memo.set(2, 1);
 
-function fib(n) {
+function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = fib(n - 1) + fib(n - 2) + fib(n - 3);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
 }
 
 const length = 40;
 for (let i = 0; i <= length; i++ ) {
-    console.log(fib(i));
+    console.log(trib(i));
 }
