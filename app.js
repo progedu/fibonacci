1f3d8dd69f22f29cb1bfda60bf9d2f1b7988d332
ShumpeiSenami <kaifenan@gmail.com>
2018-12-14 09:43:05 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -4,18 +4,19 @@
 const memo = new Map();  //連想配列　keyには順番を、valueの値には答えを格納する
 //一度計算した結果を保存しておく方法をメモ化というので変数名を memo にしている
 memo.set(0,0);   //０番目の値（答え）は０を格納
-memo.set(1,1);   //１番目の値（答え）は１を格納
+memo.set(1,0);   //１番目の値（答え）は１を格納
+memo.set(2,1);
 
-function fib(n){
+function trib(n){
     if(memo.has(n)){
         return memo.get(n);
     }
-    const value = fib(n -1)+ fib(n-2);
+    const value = trib(n-1)+ trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
     }
 
 const length = 40;
 for(let i = 0; i <= length; i++){
-    console.log(fib(i));
+    console.log(trib(i));
 }
