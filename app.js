49bb26c6a9e9428436ef9bf0715a01a8c6b47c6d
takuyam98634217 <takuyama045@gmail.com>
2019-03-02 17:42:35 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
 function fib(n) {
-    if (n === 0){
-        return 0;
-    } else if (n === 1){
-        return 1;
+    // あればその値を返却
+    if(memo.has(n)){
+        return memo.get(n);
     }
-    return fib(n-1) + fib(n-2);
+    // なければ計算して返却
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
