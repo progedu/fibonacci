00d05c9dd29aa87b48f5c8cbb7c32c8742a54b34
mag67 <uec.donburi6712@gmail.com>
2019-04-15 10:42:16 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+/*
+//フィボナッチ数列
 const memo = new Map();
 memo.set(0,0);
 memo.set(1,1);
@@ -16,4 +18,27 @@ function fib(n){
 const length=100;
 for(let i=0;i<=length;i++){
     console.log(fib(i));
+}
+*/
+
+
+//トリボナッチ数列
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,0);
+memo.set(2,1);
+
+function trib(n){
+    if(memo.has(n)){
+        return memo.get(n);
+    }
+
+    const value = trib(n-3)+trib(n-2)+trib(n-1);
+    memo.set(n,value);
+    return value;
+}
+
+const length=100;
+for(let i=0;i<=length;i++){
+    console.log(trib(i));
 }
