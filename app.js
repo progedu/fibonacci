27f2a85a33bb50cac689e6e5a3bcc728e9ae47b2
tomoko-matsuura <kitano@bombsight.net>
2019-06-10 08:12:58 +0900

    トリボナッチ数列の実装

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1)
 
-function fib (n){
-   if(memo.has(n)){
+
+function tori(n) {
+   if(memo.has(n)) {
     return memo.get(n);
    }
-   const value = fib(n-1) + fib(n-2) ;
+   const value = tori(n-1) + tori(n-2)+tori(n-3) ;
    memo.set(n, value);
    return value;
 }
@@ -15,7 +17,8 @@ function fib (n){
 const length = 40;
 
 for (let i = 0 ;i <= length; i++){
-console.log(fib(i));
+console.log(tori(i));
+	
 }
 
 
