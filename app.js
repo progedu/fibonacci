cd8d86ea089c741680f440d7b3a517be8e2bb4da
owt3 <baison0531@yahoo.co.jp>
2019-06-11 16:15:05 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 'use strict';
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 0);
-memo.set(2, 1);
-function trib(n) {
-    if(memo.has(n)) {
-        return memo.get(n);
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
     }
-    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
-    memo.set(n, value);
-    return value;
+    return fib(n - 1) + fib(n - 2);
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+    console.log(fib(i));
 }
