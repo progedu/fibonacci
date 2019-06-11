8a9c927445ce4dcc26ba9ca16a3980de3c9ef937
owt3 <baison0531@yahoo.co.jp>
2019-06-11 16:34:35 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
+    if (memo.has(n)) {
+        return memo.get(n);
+    } 
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
