2812cc4bf1132131475d26b62235609506b003d2
matchalol <rosequartz-elestial@msn.com>
2019-07-03 21:49:01 +0900

    トリボナッチ数列

--- app_bak.js
+++ app_bak.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
-// フィボナッチ数列：指数オーダー
+// 指数オーダー
 function fib(n) {
 	if (n === 0) {
 		return 0;	// 0のときはここで終了
@@ -12,8 +12,7 @@ function fib(n) {
 }
 */
 
-/*
-// フィボナッチ数列：線形オーダー
+// 線形オーダー（memo化して処理回数を1回にする）
 const memo = new Map();
 
 memo.set(0, 0);
@@ -31,31 +30,6 @@ function fib(n) {
 }
 
 const length = 500;
-
-for (let i = 0; i <= length; i++) {
-	console.log(fib(i));
-}
-*/
-
-// トリボナッチ数列
-const memo = new Map();
-
-memo.set(0, 0);
-memo.set(1, 0);
-memo.set(2, 1);
-
-function fib(n) {
-	//キーnが存在する場合（再帰で呼び出したとき）の処理
-	if (memo.has(n)) {	// hasでキーの有無を確認
-		return memo.get(n);
-	}
-	// キーnが存在しないときの処理
-	const value = fib(n - 1) + fib(n - 2) + fib(n - 3);
-	memo.set(n, value);	// 計算結果を追加する
-	return value;
-}
-
-const length = 50;
 
 for (let i = 0; i <= length; i++) {
 	console.log(fib(i));
