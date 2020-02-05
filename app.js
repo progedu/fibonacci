0a8ef90fddac20f11cb38fa0bf754b32fa36c666
”takumi <serubber666@icloud.com>
2020-02-05 08:24:11 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+function fib(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
+
+
+
+/*
+
+'use strict';
 function fib(n) {
     if (n === 0) {
         return 0;
@@ -12,7 +33,6 @@ for (let i = 0; i <= length; i++) {
     console.log(fib(i));
 }
 
-/*
 
 n=1 fib(1) = r1  =1
 
@@ -23,6 +43,6 @@ n=3 fib(3-1=2) = [fib(2-1=1) = r1 + fib(2-2=0) = r0] + fib(3-2=1) = r1   =[r1+r0
 n=4 fib(4-1=3) = {fib(3-1=2) = [fib(2-1=1) = r1 + fib(2-2=0) = r0] + fib(3-2=1) = r1}
   + fib(4-2=2) = fib(2-1=1) = r1 + fib(2-2=0) = r0                     ={[r1+r0]+r1}+r1+r0  =3
 
-...関数で関数を呼んでるので分解していって「式を増やして計算された解答の値をを増やして reuturn していく」。
+...関数で関数を呼んでるので分解していって「式を増やして計算された解答の値を増やしていく」。
 
 */
