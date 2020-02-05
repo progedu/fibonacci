be4c8210cbc042bd6e236101c5d3cbee49ce5232
”takumi <serubber666@icloud.com>
2020-02-05 07:46:42 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 'use strict';
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } //else
+    return fib(n - 1) + fib(n - 2)
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
+
+/*
+
+n=1 fib(1) = r1  =1
+
+n=2 fib(2-1=1) = r1 + fib(2-2=0) = r0   =r1+r0  =1
+
+n=3 fib(3-1=2) = [fib(2-1=1) = r1 + fib(2-2=0) = r0] + fib(3-2=1) = r1   =[r1+r0]+r1  =2
+
+n=4 fib(4-1=3) = {fib(3-1=2) = [fib(2-1=1) = r1 + fib(2-2=0) = r0] + fib(3-2=1) = r1}
+  + fib(4-2=2) = fib(2-1=1) = r1 + fib(2-2=0) = r0                     ={[r1+r0]+r1}+r1+r0  =3
+
+...関数で関数を呼んでるので分解していって「式を増やして計算された解答の値をを増やして reuturn していく」。
+
+*/
