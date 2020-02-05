480a5b5ae113cc09e0c898bc97bfd315b638d48a
”takumi <serubber666@icloud.com>
2020-02-05 08:56:31 +0000

    テトラナッチ数列

--- app.js
+++ app.js
@@ -1,48 +1,20 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
-function fib(n) {
+memo.set(1, 0);
+memo.set(2, 0);
+memo.set(3, 1);
+function ttr(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = fib(n - 1) + fib(n - 2);
+    const value = ttr(n - 1) + ttr(n - 2) + ttr(n - 3) + ttr(n - 4);
     memo.set(n, value);
     return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
+    console.log(ttr(i));
 }
 
 
-
-/*
-
-'use strict';
-function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    } //else
-    return fib(n - 1) + fib(n - 2)
-}
-const length = 40;
-for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
-}
-
-
-n=1 fib(1) = r1  =1
-
-n=2 fib(2-1=1) = r1 + fib(2-2=0) = r0   =r1+r0  =1
-
-n=3 fib(3-1=2) = [fib(2-1=1) = r1 + fib(2-2=0) = r0] + fib(3-2=1) = r1   =[r1+r0]+r1  =2
-
-n=4 fib(4-1=3) = {fib(3-1=2) = [fib(2-1=1) = r1 + fib(2-2=0) = r0] + fib(3-2=1) = r1}
-  + fib(4-2=2) = fib(2-1=1) = r1 + fib(2-2=0) = r0                     ={[r1+r0]+r1}+r1+r0  =3
-
-...関数で関数を呼んでるので分解していって「式を増やして計算された解答の値を増やしていく」。
-
-*/
