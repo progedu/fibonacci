d046ee760babd9b3b3f12ae1d23c6908a5dc2d68
starstar141 <exceed99200@yahoo.co.jp>
2020-02-19 06:44:48 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+function fibonacci(n){
+    if (n === 0){
+        return 0;
+    } else if (n === 1){
+        return 1;
+    } 
+    return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log(fibonacci(i));
+}
