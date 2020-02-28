e3db80c4d485a488d8f675c58690bf2bfb437d10
taku1010 <takumi.guitar1010@gmail.com>
2020-02-28 22:44:38 +0900

    トリボナッチ数列に改良

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict';
 const memo = new Map();
-memo.set(0,0);
-memo.set(1,1);
-function fib(n){
-    if(memo.has(n)){
-        return memo.get(0);
-    }
-    const value = fib(n-1) + fib(n-2);
-    memo.set(n,value);
-    return value;
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+function fib(n) {
+  if(memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = fib(n - 1) + fib(n - 2) + fib(n-3);
+  memo.set(n, value);
+  return value;
 }
-
 const length = 40;
-for(let i = 0; i <= length; i++){
-    console.log(fib(i));
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
 }
