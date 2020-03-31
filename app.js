64c21967c32e9d0b7f201ad9cbce72e6f94dead3
funahashi_home_ubuntu <fnhs653@gmail.com>
2020-03-31 13:35:29 +0900

    関数をトリボナッチ数列に対応するよう改変

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 'use strict';
 const memo = new Map();
 memo.set(0,0);
-memo.set(1,1);
+memo.set(1,0);
+memo.set(2,1);
 
 function fib(n) {
     if (memo.has(n)){
         return memo.get(n);
     }
-    memo.set(n,fib(n-1) + fib(n-2));
+    memo.set(n,fib(n-1) + fib(n-2) + fib(n-3));
     return memo.get(n);
 }
 const length = 40;
