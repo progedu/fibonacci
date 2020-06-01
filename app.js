73435cf7c503ae7950d27fa4b3dbc78b32a2bfcc
Harumi <hru.o6o4@gmail.com>
2020-06-01 17:45:40 +0900

    フィボナッチ数列をトリボナッチ数列に変更

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 "use strict";
 
+// Map関数の作成。
 const memo = new Map();
+//n=2まで初期値をセット
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
 
-function fib(n) {
+/**
+ *トリボナッチ数列
+ * @param {Number} n トリボナッチ数列に渡す整数
+ */
+
+function trib(n) {
   if (memo.has(n)) {
     return memo.get(n);
   }
-  const value = fib(n - 1) + fib(n - 2);
+  const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
   memo.set(n, value);
   return value;
 }
 
-const length = 100;
+const length = 50;
 for (let i = 0; i <= length; i++) {
-  console.log(i + "番目 " + fib(i));
+  console.log(i + "番目 " + trib(i));
 }
