86200edebc9505f467ec7daeb2bbac83d0f6651b
okayu-smb <kanamono32@gmail.com>
2020-06-04 21:43:54 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict';
-function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-     return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+function trib(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    } 
+    const value = trib(n - 1) + trib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
+    console.log(trib(i));
 }
