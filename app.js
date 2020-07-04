bbf8f5af2088af13c0450489d24361bb98c25a81
Kobayashi Tomoya <tomocha.marika@gmail.com>
2020-07-04 15:22:15 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// 0のときは0を返す
+function fibonacci(n) {
+  if(n == 0){
+    return 0;
+  }else if(n == 1){
+    return 1;
+  }
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+for (var i = 0; i <= 40; i++) {
+  console.log(fibonacci(i));
+}
