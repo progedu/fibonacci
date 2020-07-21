e88f96c240a7cdf64c9c69e9af34e03a902599a4
Kohei Kusumoto <tnrfomqzop@yahoo.co.jp>
2020-07-21 21:17:12 +0900

    トリボナッチ数列を計算する関数を追加

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+// フィボナッチ数列関数
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
@@ -10,7 +11,20 @@ function fib(n) {
   memo.set(n, value);
   return value;
 }
+// トリボナッチ数列関数
+const tribMap = new Map();
+tribMap.set(0, 0);
+tribMap.set(1, 0);
+tribMap.set(2, 1);
+function trib(n) {
+  if (tribMap.has(n)) {
+    return tribMap.get(n);
+  }
+  const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+  tribMap.set(n, value);
+  return value;
+}
 const length = 40;
 for (let i = 0; i <= length; i++) {
-  console.log(fib(i));
+  console.log(trib(i));
 }
