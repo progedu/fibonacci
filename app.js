6e7e5c6f505f8be62c75a8b284d44dccb2c040b5
Asahi <widegain.med@gmail.com>
2020-08-04 02:55:03 +0000

    トリボナッチ数列の実装

--- app.js
+++ app.js
@@ -2,15 +2,16 @@
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
-function fib(n) {
+memo.set(2, 1);
+function tfib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = fib(n - 1) + fib(n - 2);
+    const value = tfib(n - 1) + tfib(n - 2) + tfib(n - 3);
     memo.set(n, value);
     return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
+    console.log(tfib(i));
 }
