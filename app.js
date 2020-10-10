eee4367b92bf7fc36d625a2a76c6fa00a4a64c52
山下元基 <motoki.yamashita.me@gmail.com>
2020-10-10 17:39:13 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n) {
+    if (n === 0) {
+      return 0;
+    } else if (n === 1) {
+      return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+  }
+  const length = 40;
+  for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+  }
