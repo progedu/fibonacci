167678c2a974d1ec0b02cf5050b25456cc1662f4
aozametatuki <akomatsu1224@gmail.com>
2021-01-06 00:49:32 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 'use strict';
 
+// memo変数に順番ごとの値を格納する
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n){
-    // 0の場合は0を返す
-    if(n === 0){
-        return 0;
-    // 1の場合は1を返す
-    }else if(n === 1){
-        return 1;
+    // すでにmemoに格納されている場合,その値を返す
+    if(memo.has(n)){
+        return memo.get(n);
     }
+
     // 再帰関数(自分自身を呼び出す)
-    return fib(n-1) + fib(n-2);
+    const value =  fib(n-1) + fib(n-2);
+    // 計算結果を順番と共にmemoに格納する
+    memo.set(n, value);
+
+    return value;
 }
 
-const length = 40;
+const length = 100;
 for(let i = 0; i<=length; i++){
     console.log(fib(i));
 }
