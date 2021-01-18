edaa51a7010cb52f1fb14d6ba1e4ea4f84d02e3b
Yusuke Abe <abe.infocity.co.jp>
2021-01-18 18:26:07 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 'use strict';
-function fib(n){
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+function fib(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+  memo.set(n, value);
+  return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(trib(i));
+}
+
+
+
+
+
+
+
+/**function fib(n){
     if (n === 0){
      return 0; 
     }else if(n === 1){
@@ -10,4 +33,4 @@ function fib(n){
 const length = 40;
 for (let i = 0; i <= length; i++){
     console.log(fib(i));
-}
+}/
