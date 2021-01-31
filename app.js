61bdb902a7b37d96acce122961e52d4002707eba
shota morita <kokoro7791@gmail.com>
2021-01-31 10:51:18 +0900

    O記法のfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+console.time('fib');
+function fib(n){
+  if(n === 0){
+    return 0;
+  }else if(n === 1){
+    return 1;
+  }
+  return fib(n - 1)+ fib(n - 2);
+}
+let l = process.argv[2] || 10;
+for(let i = 0; i <= l; i++){
+  console.log(fib(i));
+}
+console.timeEnd('fib');
