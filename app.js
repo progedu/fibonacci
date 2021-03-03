b35c6684961cfcef0dca8777ef18d5d6255eea08
Yuzuko Niigaki <yozukon@array.co.jp>
2021-03-03 11:59:54 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+function fib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
