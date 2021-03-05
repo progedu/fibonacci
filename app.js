127201668710e11168a80a1c2cc37ef06615af93
Selno <meitxyps385@gmail.com>
2021-03-06 00:03:47 +0900

    指数オーダーのアルゴリズムです

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 'use strict';
+/* function fibonacci(n) {
+    // 0番目の値は無条件に0にしておこう。for文を嚙ますまでもない
+    if (n === 0) {
+        return 0
+    }
+    let array = [0, 1];
+    for (let i = 2; i < n; i++) {
+        array.push(array[i-1] + array[i-2]);
+    }
+    return array[n-1]
+    console.log(fibonacci(100));
+} */
+
+function fib(n) {
+    if (n === 0) {
+      return 0;
+    } else if (n === 1) {
+      return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+  }
+
+
+console.log(fib(40));
