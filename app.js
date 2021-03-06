dd9713463d3a449612364224e0e4f3c9c2038958
Selno <meitxyps385@gmail.com>
2021-03-06 11:41:49 +0900

    フィボナッチ数の計算と出力機能を作った

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
-/* function fibonacci(n) {
-    // 0番目の値は無条件に0にしておこう。for文を嚙ますまでもない
+// 自分が考えたやつ
+/* function fib(n) {
+    // 0番目の値は無条件に0。for文を嚙ますまでもない
     if (n === 0) {
         return 0
     }
@@ -9,17 +10,32 @@
         array.push(array[i-1] + array[i-2]);
     }
     return array[n-1]
-    console.log(fibonacci(100));
 } */
 
-function fib(n) {
+// 教材の悪い例
+/*function fib(n) {
     if (n === 0) {
       return 0;
     } else if (n === 1) {
       return 1;
     }
     return fib(n - 1) + fib(n - 2);
-  }
+}
+console.log(fib(40)); */
 
-
-console.log(fib(40));
+var data = new Map();
+data.set(0, 0);
+data.set(1, 1);
+/**
+ * @param {number} n 
+ * @returns {number}
+ */
+function fib(n) {
+    if ( data.has(n) ) {
+        return data.get(n)
+    }
+    const value = fib(n-1)+fib(n-2);
+    data.set(n, value);
+    return value
+}
+console.log( fib(process.argv[2]) );
