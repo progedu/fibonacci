fbcc1d8cccc2b578ccfa4e6e4109eca818262f0b
prin <tuboprin@gmail.com>
2021-03-21 18:30:13 +0900

    トリボナッチ数列の実装

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 'use strict';
 const memo=new Map();
 memo.set(0,0);
-memo.set(1,1);
+memo.set(1,0);
+memo.set(2,1);
 function fib(n){
     if(memo.has(n)){
         return memo.get(n);
     }
-    const value=fib(n-1)+fib(n-2);
+    const value=fib(n-1)+fib(n-2)+fib(n-3);
     memo.set(n,value);
     return value;
 }
