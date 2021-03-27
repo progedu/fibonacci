868ffd13cb928f95019c0292de05fa8caa9fd8e1
reicch <rei_20n1100178@nnn.ed.jp>
2021-03-27 13:46:49 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+function fib(n){
+if (n===0){
+    return 0;
+    }else if (n===1){
+    return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+for (let i = 0; i <= 40; i++){
+    console.log( i + "のフィボナッチ数は" + fib(i) + "です。");
+}
