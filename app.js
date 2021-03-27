a13f9108dcf501ffde660050154b9bee7a3fdcb2
reicch <rei_20n1100178@nnn.ed.jp>
2021-03-27 14:36:04 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 'use strict';
+const memo = new Map();
+//n=0,n=1は最初から決まってる
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n){
-if (n===0){
-    return 0;
-    }else if (n===1){
-    return 1;
+    if (memo.has(n)){
+    return memo.get(n);
     }
-    return fib(n-1) + fib(n-2);
+    let value = fib(n-1) + fib(n-2);
+    memo.set(n, value);
+    return value;
 }
-for (let i = 0; i <= 40; i++){
+for (let i = 0; i <= 100; i++){
     console.log( i + "のフィボナッチ数は" + fib(i) + "です。");
 }
