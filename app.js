2a3fb2f793583ad39f55ca61d76c11079cdfa604
reicch <rei_20n1100178@nnn.ed.jp>
2021-03-28 14:07:59 +0900

    トリボナッチのアリゴリズムに変更

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 const memo = new Map();
 //n=0,n=1は最初から決まってる
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
 function fib(n){
     if (memo.has(n)){
     return memo.get(n);
     }
-    let value = fib(n-1) + fib(n-2);
+    let value = fib(n-1) + fib(n-2) + fib(n-3);
     memo.set(n, value);
     return value;
 }
-for (let i = 0; i <= 100; i++){
-    console.log( i + "のフィボナッチ数は" + fib(i) + "です。");
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log( i + "のトリボナッチ数は" + fib(i) + "です。");
 }
